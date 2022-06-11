3060eade86d1cbf7eabaff0b40bebb6d03f9f0ef
Zefira Shannon <earthlingzephyr@gmail.com>
2022-06-10 20:44:10 -0700

    Wrap reqwest client and auth_token in our own Client

--- src/main.rs
+++ src/main.rs
@@ -17,23 +17,41 @@ fn main() {
 
 #[tokio::main()]
 async fn run() {
-    send_file("q.ns", "hello world").await;
+    let client = Client::new("");
+    client.send_file("q.ns", "hello world").await;
 }
 
-async fn send_file(filename: impl Into<String>, body: impl Into<String>) {
-    let mut file_data: HashMap<&'static str, String> = HashMap::new();
-    file_data.insert("filename", filename.into());
-    file_data.insert("code", base64::encode(body.into()));
+#[derive(Clone, Debug)]
+struct Client {
+    client: reqwest::Client,
+    auth_token: String,
+}
 
-    let client = reqwest::Client::new();
-    let res = client
-        .put("http://localhost:9990/")
-        .bearer_auth("")
-        .json(&file_data)
-        .send()
-        .await
-        .expect("failed to send file");
-    println!("response: {:#?}", res);
-    let body_json: serde_json::Value = res.json().await.expect("failed to receive response body");
-    println!("body: {:#?}", body_json);
+impl Client {
+    pub fn new(auth_token: impl Into<String>) -> Self {
+        let client = reqwest::Client::new();
+        Self {
+            client,
+            auth_token: auth_token.into(),
+        }
+    }
+
+    async fn send_file(&self, filename: impl Into<String>, body: impl Into<String>) {
+        let mut file_data: HashMap<&'static str, String> = HashMap::new();
+        file_data.insert("filename", filename.into());
+        file_data.insert("code", base64::encode(body.into()));
+
+        let res = self
+            .client
+            .put("http://localhost:9990/")
+            .bearer_auth(&self.auth_token)
+            .json(&file_data)
+            .send()
+            .await
+            .expect("failed to send file");
+        println!("response: {:#?}", res);
+        let body_json: serde_json::Value =
+            res.json().await.expect("failed to receive response body");
+        println!("body: {:#?}", body_json);
+    }
 }
