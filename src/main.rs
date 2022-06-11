8501d7b6aec5d7148243b7d55c36dfa05f12d11f
Zefira Shannon <earthlingzephyr@gmail.com>
2022-06-10 21:26:21 -0700

    Walk the base directory correctly filtering out unneeded stuff

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
-use std::{collections::HashMap, path::PathBuf};
+use std::{
+    collections::HashMap,
+    path::{Path, PathBuf},
+};
 
 use clap::Parser;
-
-#[allow(dead_code)]
-static VALID_EXTENSIONS: [&str; 3] = [".js", ".script", ".ns"];
+use walkdir::DirEntry;
 
 #[derive(Parser, Debug)]
 struct Args {
@@ -11,34 +12,80 @@ struct Args {
 }
 
 fn main() {
-    let _args = Args::parse();
-    run();
+    let args = Args::parse();
+    run(args);
+}
+
+fn is_hidden(entry: &DirEntry) -> bool {
+    entry
+        .file_name()
+        .to_str()
+        .map(|s| s.starts_with("."))
+        .unwrap_or(false)
+}
+
+static VALID_EXTENSIONS: [&str; 3] = [".js", ".script", ".ns"];
+
+fn is_valid(entry: &DirEntry) -> bool {
+    // Don't skip directories or walking won't happen as the root
+    // directory will be skipped
+    if entry.file_type().is_dir() {
+        return true;
+    }
+
+    entry
+        .file_name()
+        .to_str()
+        .map(|s| {
+            dbg!(s);
+            for ext in &VALID_EXTENSIONS {
+                if s.ends_with(ext) {
+                    dbg!(ext);
+                    return true;
+                }
+            }
+            false
+        })
+        .unwrap_or(false)
 }
 
 #[tokio::main()]
-async fn run() {
-    let client = Client::new("");
-    client.send_file("q.ns", "hello world").await;
+async fn run(args: Args) {
+    let client = Client::new("", args.sync_dir.clone());
+
+    let walker = walkdir::WalkDir::new(&args.sync_dir)
+        .follow_links(false)
+        .into_iter()
+        .filter_entry(|e| !is_hidden(e) && is_valid(e));
+    for entry in walker {
+        let entry = entry.expect("failed while walking directory");
+        if entry.file_type().is_file() {
+            println!("entry path {}", entry.path().display());
+            client.send_file(entry.path(), "hello world").await;
+        }
+    }
 }
 
 #[derive(Clone, Debug)]
 struct Client {
     client: reqwest::Client,
     auth_token: String,
+    base_directory: PathBuf,
 }
 
 impl Client {
-    pub fn new(auth_token: impl Into<String>) -> Self {
+    pub fn new(auth_token: impl Into<String>, base_directory: PathBuf) -> Self {
         let client = reqwest::Client::new();
         Self {
             client,
             auth_token: auth_token.into(),
+            base_directory,
         }
     }
 
-    async fn send_file(&self, filename: impl Into<String>, body: impl Into<String>) {
+    async fn send_file(&self, filename: &Path, body: impl Into<String>) {
         let mut file_data: HashMap<&'static str, String> = HashMap::new();
-        file_data.insert("filename", filename.into());
+        file_data.insert("filename", self.munge_filename(filename));
         file_data.insert("code", base64::encode(body.into()));
 
         let res = self
@@ -53,5 +100,14 @@ impl Client {
         let body_json: serde_json::Value =
             res.json().await.expect("failed to receive response body");
         println!("body: {:#?}", body_json);
+    }
+
+    fn munge_filename(&self, path: &Path) -> String {
+        dbg!(&self.base_directory, path);
+        let relative_path = path
+            .strip_prefix(&self.base_directory)
+            .expect("failed to strip prefix");
+
+        relative_path.display().to_string()
     }
 }
