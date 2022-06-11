f351f7dfdcde9aede1b1096ac479c3412efed674
Zefira Shannon <earthlingzephyr@gmail.com>
2022-06-10 20:24:07 -0700

    Get async client working

--- src/main.rs
+++ src/main.rs
@@ -4,19 +4,21 @@ static VALID_EXTENSIONS: [&str; 3] = [".js", ".script", ".ns"];
 
 // Connect to http://localhost:9990
 // postURI: '/'
-fn main() {
+#[tokio::main()]
+async fn main() {
     let mut file_data: HashMap<&'static str, &'static str> = HashMap::new();
     file_data.insert("filename", "file.script");
     file_data.insert("code", "SGVsbG8=");
 
-    let client = reqwest::blocking::Client::new();
+    let client = reqwest::Client::new();
     let res = client
         .put("http://localhost:9990/")
         .bearer_auth("")
         .json(&file_data)
         .send()
+        .await
         .expect("failed to send file");
     println!("response: {:#?}", res);
-    let body_json: serde_json::Value = res.json().expect("failed to receive response body");
+    let body_json: serde_json::Value = res.json().await.expect("failed to receive response body");
     println!("body: {:#?}", body_json);
 }
