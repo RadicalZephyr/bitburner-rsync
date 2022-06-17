843466deca1313c04971d89c9d20470a1bbb0129
Zefira Shannon <earthlingzephyr@gmail.com>
2022-06-17 05:08:29 -0700

    Add needed cargo-out-dir script and infrastructure

---
+++ build.rs
@@ -0,0 +1,23 @@
+use std::{env, fs, path, process};
+
+fn main() {
+    // OUT_DIR is set by Cargo and it's where any additional build artifacts
+    // are written.
+    let outdir = match env::var_os("OUT_DIR") {
+        Some(outdir) => outdir,
+        None => {
+            eprintln!(
+                "OUT_DIR environment variable not defined. \
+                 Please file a bug: \
+                 https://github.com/RadicalZephyr/bitburner-rsync/issues/new"
+            );
+            process::exit(1);
+        }
+    };
+    fs::create_dir_all(&outdir).unwrap();
+
+    let stamp_path = path::Path::new(&outdir).join("bb-rsync-stamp");
+    if let Err(err) = fs::File::create(&stamp_path) {
+        panic!("failed to write {}: {}", stamp_path.display(), err);
+    }
+}
